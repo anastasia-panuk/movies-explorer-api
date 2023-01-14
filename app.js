1382ce806507a43963b67d15a4bff514406986c9
Anastasia Panuk <AnaPanana@yandex.ru>
2023-01-14 19:34:40 +0300

    fix: правки по ревью

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv');
 require('dotenv').config();
 const { errors } = require('celebrate');
 const cors = require('cors');
-const helmet = require('helmet');
 const { errorHandler } = require('./middlewares/errorHandler');
 
 const router = require('./routes/index');
@@ -37,8 +36,6 @@ app.use(cors({
 app.use(express.json());
 
 app.use(requestLogger);
-
-app.use(helmet());
 
 app.use(router);
 
